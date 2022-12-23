b6914ffa6dd037b81ab605d1f9d5073c3c1644c8
Jamal Aartsen <jamal.aartsen@wearetriple.com>
2022-12-23 11:17:14 +0100

    second commit

---
+++ PokyMon/PokyMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokyMonApp.swift
+//  PokyMon
+//
+//  Created by Jamal Aartsen on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokyMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
